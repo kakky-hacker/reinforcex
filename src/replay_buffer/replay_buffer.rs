6bddae04876bcc1946fabf556b299d7c8df13bf5
TakumaKakinoue <kakinouet@gmail.com>
2025-02-23 17:45:10 +0900

    fix:replay_buffer

--- src/replay_buffer/replay_buffer.rs
+++ src/replay_buffer/replay_buffer.rs
@@ -13,10 +13,9 @@ pub struct ReplayBuffer {
 pub struct Experience {
     pub state: Tensor,
     pub action: Option<Tensor>,
-    pub reward: f64,
-    pub q_value: RefCell<Option<f64>>,
-    pub next_experience: RefCell<Option<Rc<Experience>>>,
-    prev_n_experiences: Vec<Rc<Experience>>,
+    pub reward_for_this_state: f64,
+    pub n_step_discounted_reward: RefCell<Option<f64>>,
+    pub n_step_after_experience: RefCell<Option<Rc<Experience>>>,
 }
 
 impl ReplayBuffer {
@@ -34,39 +33,40 @@ impl ReplayBuffer {
         state: Tensor,
         action: Option<Tensor>,
         reward: f64,
-        is_state_terminal: bool,
+        is_episode_terminal: bool,
         gamma: f64,
     ) {
-        let prev_n_experiences: Vec<Rc<Experience>> = self.last_n_experiences.clone().into_iter().collect();
-
         let experience = Rc::new(Experience {
             state,
             action,
-            reward,
-            q_value: RefCell::new(None),
-            next_experience: RefCell::new(None),
-            prev_n_experiences: prev_n_experiences.clone(),
+            reward_for_this_state: reward,
+            n_step_discounted_reward: RefCell::new(None),
+            n_step_after_experience: RefCell::new(None),
         });
     
+        let prev_n_experiences: Vec<Rc<Experience>> = self.last_n_experiences.clone().into_iter().collect();
+
         if !prev_n_experiences.is_empty() {
-            let mut rewards: Vec<f64> = prev_n_experiences.iter().map(|e| e.reward).collect();
+            let mut rewards: Vec<f64> = prev_n_experiences.iter().skip(1).map(|e| e.reward_for_this_state).collect();
             rewards.push(reward);
-            let q_value = cumsum::cumsum_rev(&rewards, gamma)[0];
-            *prev_n_experiences[0].q_value.borrow_mut() = Some(q_value);
-            *prev_n_experiences[prev_n_experiences.len() - 1].next_experience.borrow_mut() = Some(Rc::clone(&experience));
+            let n_step_discounted_reward = cumsum::cumsum_rev(&rewards, gamma)[0];
+            *prev_n_experiences[0].n_step_discounted_reward.borrow_mut() = Some(n_step_discounted_reward);
+            *prev_n_experiences[0].n_step_after_experience.borrow_mut() = Some(Rc::clone(&experience));
         }
 
         self.last_n_experiences.push_back(Rc::clone(&experience));
-        self.memory.append(experience);
 
-        if is_state_terminal {
+        if is_episode_terminal {
             let prev_experiences: Vec<Rc<Experience>> = self.last_n_experiences.clone().into_iter().collect();
-            let rewards: Vec<f64> = prev_experiences.iter().map(|e| e.reward).collect();
+            let mut rewards: Vec<f64> = prev_experiences.iter().skip(1).map(|e| e.reward_for_this_state).collect();
+            rewards.push(0.0);  // For terminal state
             let q_values = cumsum::cumsum_rev(&rewards, gamma);
-            for (experience, &q_value) in prev_experiences.iter().zip(q_values.iter()) {
-                *experience.q_value.borrow_mut() = Some(q_value);
+            for (experience, &n_step_discounted_reward) in prev_experiences.iter().zip(q_values.iter()) {
+                *experience.n_step_discounted_reward.borrow_mut() = Some(n_step_discounted_reward);
             }
             self.last_n_experiences.empty();
+        } else {
+            self.memory.append(experience);
         }
     }
 
@@ -133,7 +133,7 @@ mod tests {
         let state8 = Tensor::from_slice(&[7.0]);
         let state9 = Tensor::from_slice(&[8.0]);
 
-        buffer.append(state1, None, 1.0, false, 0.9);
+        buffer.append(state1, None, 0.0, false, 0.9);
         buffer.append(state2, None, 2.0, false, 0.9);
         buffer.append(state3, None, 3.0, true, 0.9);
         buffer.append(state4, None, 0.0, false, 0.9);
@@ -144,40 +144,40 @@ mod tests {
         buffer.append(state9, None, 5.0, true, 0.9);
 
         for experience in buffer.sample(200) {
-            let q_value = *experience.q_value.borrow();
-            let next_experience = experience.next_experience.borrow();
+            let n_step_discounted_reward = *experience.n_step_discounted_reward.borrow();
+            let n_step_after_experience = experience.n_step_after_experience.borrow();
             let expected_q_value;
             if experience.state.double_value(&[]) == 0.0 {
-                expected_q_value = 1.0 + 0.9 * 2.0 + 0.9 * 0.9 * 3.0;
-                assert!(next_experience.is_some());
-            } else if experience.state.double_value(&[]) == 1.0 {
                 expected_q_value = 2.0 + 0.9 * 3.0;
-                assert!(next_experience.is_some());
-            } else if experience.state.double_value(&[]) == 2.0 {
+                assert!((n_step_discounted_reward.unwrap() - expected_q_value).abs() < 1e-6);
+                assert!(n_step_after_experience.is_some());
+            } else if experience.state.double_value(&[]) == 1.0 {
                 expected_q_value = 3.0;
-                assert!(next_experience.is_none());
+                assert!((n_step_discounted_reward.unwrap() - expected_q_value).abs() < 1e-6);
+                assert!(n_step_after_experience.is_none());
             } else if experience.state.double_value(&[]) == 3.0 {
                 expected_q_value = 0.0;
-                assert!(next_experience.is_some());
+                assert!((n_step_discounted_reward.unwrap() - expected_q_value).abs() < 1e-6);
+                assert!(n_step_after_experience.is_some());
             } else if experience.state.double_value(&[]) == 4.0 {
                 expected_q_value = 0.0;
-                assert!(next_experience.is_some());
+                assert!((n_step_discounted_reward.unwrap() - expected_q_value).abs() < 1e-6);
+                assert!(n_step_after_experience.is_some());
             } else if experience.state.double_value(&[]) == 5.0 {
                 expected_q_value = 0.0;
-                assert!(next_experience.is_some());
+                assert!((n_step_discounted_reward.unwrap() - expected_q_value).abs() < 1e-6);
+                assert!(n_step_after_experience.is_some());
             } else if experience.state.double_value(&[]) == 6.0 {
-                expected_q_value = 0.9 * 0.9 * 5.0;
-                assert!(next_experience.is_some());
-            } else if experience.state.double_value(&[]) == 7.0 {
                 expected_q_value = 0.9 * 5.0;
-                assert!(next_experience.is_some());
-            } else if experience.state.double_value(&[]) == 8.0 {
+                assert!((n_step_discounted_reward.unwrap() - expected_q_value).abs() < 1e-6);
+                assert!(n_step_after_experience.is_some());
+            } else if experience.state.double_value(&[]) == 7.0 {
                 expected_q_value = 5.0;
-                assert!(next_experience.is_none());
+                assert!((n_step_discounted_reward.unwrap() - expected_q_value).abs() < 1e-6);
+                assert!(n_step_after_experience.is_none());
             } else {
-                expected_q_value = -1.0;
+                panic!("Unexpected state")
             }
-            assert!((q_value.unwrap() - expected_q_value).abs() < 1e-6);
         }
     }
 }
