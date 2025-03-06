f6de79c5cdd67ad12ffd24c31c79cff004c5e307
kakky-hacker <kakinouet@gmail.com>
2025-03-06 23:18:33 +0900

    update:BasePolicyNetwork

--- src/models.rs
+++ src/models.rs
@@ -1,15 +1,15 @@
-//mod policy_based {
-//pub mod base_policy_network;
-//pub mod fc_gaussian_policy;
-//pub mod fc_softmax_policy;
-//}
+mod policy_based {
+    pub mod base_policy_network;
+    //pub mod fc_gaussian_policy;
+    //pub mod fc_softmax_policy;
+}
 
 mod value_based {
     pub mod base_q_network;
     pub mod fc_q_network;
 }
 
-//pub use policy_based::base_policy_network::BasePolicy;
+pub use policy_based::base_policy_network::BasePolicy;
 //pub use policy_based::fc_gaussian_policy::{FCGaussianPolicy, FCGaussianPolicyWithValue};
 //pub use policy_based::fc_softmax_policy::{FCSoftmaxPolicy, FCSoftmaxPolicyWithValue};
 
