290232173c559b4ea608ff72ee3f28c87239d089
TakumaKakinoue <kakinouet@gmail.com>
2025-04-06 18:01:21 +0900

    update:ppo

--- src/agents/ppo.rs
+++ src/agents/ppo.rs
@@ -12,6 +12,7 @@ pub struct PPO {
     update_interval: usize,
     transition_buffer: TransitionBuffer,
     epoch: usize,
+    n_steps: usize,
     t: usize,
 }
 
@@ -31,6 +32,7 @@ impl PPO {
             update_interval,
             transition_buffer: TransitionBuffer::new(100000000, n_steps),
             epoch,
+            n_steps,
             t: 0,
         }
     }
@@ -41,17 +43,17 @@ impl PPO {
             .sample(self.transition_buffer.len(), false);
         let mut states: Vec<Tensor> = vec![];
         let mut n_step_after_states: Vec<Tensor> = vec![];
-        let mut n_step_after_actions: Vec<Tensor> = vec![];
         let mut actions: Vec<Tensor> = vec![];
         let mut n_step_discounted_rewards: Vec<f64> = vec![];
         for experience in experiences {
             let state = experience.state.shallow_clone();
-            let n_step_after_experience = experience
+            let n_step_after_state = experience
                 .n_step_after_experience
                 .borrow()
                 .as_ref()
-                .unwrap();
-            let n_step_after_state = n_step_after_experience.state.shallow_clone();
+                .unwrap()
+                .state
+                .shallow_clone();
             let action = experience.action.as_ref().unwrap().shallow_clone();
             let n_step_discounted_reward = experience
                 .n_step_discounted_reward
@@ -62,27 +64,43 @@ impl PPO {
             actions.push(action);
             n_step_discounted_rewards.push(n_step_discounted_reward);
         }
-        for _ in range(self.epoch) {
-            let (action_distribs, values) = self.model.forward(&states);
-            let td_errors = self._compute_td_error(values, n_step_discounted_rewards);
-            let loss = self._compute_policy_loss(action_distribs, actions, td_errors) + td_errors.square();
+        let _batch_states = batch_states(&states, self.model.is_cuda());
+        let _batch_n_step_after_states = batch_states(&n_step_after_states, self.model.is_cuda());
+        let _batch_actions = batch_states(&actions, self.model.is_cuda());
+        for _ in 0..self.epoch {
+            let (action_distribs, values) = self.model.forward(&_batch_states);
+            let td_errors = self._compute_td_error(
+                values.unwrap(),
+                &n_step_discounted_rewards,
+                &_batch_n_step_after_states,
+            );
+            let loss = self._compute_policy_loss(action_distribs, &_batch_actions, &td_errors)
+                + td_errors.square().mean(tch::Kind::Float);
             self.optimizer.zero_grad();
             loss.backward();
             self.optimizer.step();
         }
     }
 
-    fn _compute_td_error(&self, values: Tensor, n_step_discounted_rewards: Vec<f64>, n_step_after_states: &Vec<Tensor>) -> Tensor {
-        assert_eq!(n_step_after_states.len(), n_step_discounted_rewards.len());
-        let _states = batch_states(n_step_after_states, self.model.is_cuda());
-        let (_, pred_values) = self.model.forward(&_states);
+    fn _compute_td_error(
+        &self,
+        values: Tensor,
+        n_step_discounted_rewards: &Vec<f64>,
+        n_step_after_states: &Tensor,
+    ) -> Tensor {
+        let (_, pred_values) = self.model.forward(&n_step_after_states);
         let n_step_discounted_rewards_tensor = Tensor::from_slice(&n_step_discounted_rewards);
         let gamma_n = self.gamma.powi(self.n_steps as i32);
-        let td_error = n_step_discounted_rewards_tensor + pred_values * gamma_n - values;
+        let td_error = n_step_discounted_rewards_tensor + pred_values.unwrap() * gamma_n - values;
         td_error
     }
 
-    fn _compute_policy_loss(&self, action_distribs, actions, td_errors) -> Tensor {
+    fn _compute_policy_loss(
+        &self,
+        action_distribs: Box<dyn BaseDistribution>,
+        actions: &Tensor,
+        td_errors: &Tensor,
+    ) -> Tensor {
         let log_probs = action_distribs.log_prob(actions);
         let policy_loss = -1.0 * log_probs * td_errors.detach();
         policy_loss
