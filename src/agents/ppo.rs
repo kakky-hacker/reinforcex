65987eb7890ef18665f11ff42a94a05cfcd3f302
TakumaKakinoue <kakinouet@gmail.com>
2025-07-02 00:55:31 +0900

    refactor:code

--- src/agents/ppo.rs
+++ src/agents/ppo.rs
@@ -153,6 +153,14 @@ impl BaseAgent for PPO {
         self.transition_buffer
             .append(state, None, reward, true, self.gamma);
     }
+
+    fn get_statistics(&self) -> Vec<(String, f64)> {
+        vec![]
+    }
+
+    fn save(&self, dirname: &str, ancestors: std::collections::HashSet<String>) {}
+
+    fn load(&self, dirname: &str, ancestors: std::collections::HashSet<String>) {}
 }
 
 #[cfg(test)]
